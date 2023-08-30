6a288fed04ef633d87e48c24ec83b7da52e0b86f
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-08-30 18:52:28 +0100

    Added Functions to help clean code, fixed bug with data

--- HigherLowerGame.py
+++ HigherLowerGame.py
@@ -1,6 +1,7 @@
 #Breakdown the todo list into comments
-import random,  art
+import random,  art, os
 from game_data import data
+cls = lambda: os.system('clear')
 
 #Selecting random account, displaying details to set up game
 score = 0
@@ -20,65 +21,67 @@ description_b = b["description"]
 country_b = b["country"]
 followers_b = b["follower_count"]
 
-
-
 def replace_a(dct):
+    """Replace the content of dictionary A, with B"""
     dct.clear()
     dct.update(b)
 
-# def update_details(dict):
-#     global name
-#     name = dict["name"]
-#     # description = dict["description"]
-#     # country = dict["country"]
-#     # followers = dict["follower_count"]
+def update_a(a, b):
+    """Update the variables with the new data"""
+    global name_a
+    global description_a
+    global country_a
+    global followers_a
+    name_a = a["name"]
+    description_a = a["description"]
+    country_a = a["country"]
+    followers_a = a["follower_count"]
+    global name_b
+    global description_b
+    global country_b
+    global followers_b
+    name_b = b["name"]
+    description_b = b["description"]
+    country_b = b["country"]
+    followers_b = b["follower_count"]
+
 
 endgame = False
 while not endgame:
 
-    print(f"Compare A: {name_a}, {description_a}, from {country_a}, answer = {followers_a}")
-    print(f"Compare B: {name_b}, {description_b}, from {country_b}, answer = {followers_b}")
-    guess = input("Who has more followers? Type 'A' or 'B' ").lower()
-    if guess == "a":
-        if followers_a > followers_b:
-            score += 1
-            replace_a(a)
-            name_a = a["name"]
-            description_a = a["description"]
-            country_a = a["country"]
-            followers_a = a["follower_count"]
-            b = random.choice(data)
-            name_b = b["name"]
-            description_b = b["description"]
-            country_b = b["country"]
-            followers_b = b["follower_count"]
-            print(f"You're right! Current score: {score}")
-        else:
-            #Clear screen & end loop
-            print(f"Sorry, that's wrong. Finale score: {score}")
-            endgame = True
-    elif guess == "b":
-        if followers_b > followers_a:
-            score += 1
-            replace_a(a)
-            name_a = a["name"]
-            description_a = a["description"]
-            country_a = a["country"]
-            followers_a = a["follower_count"]
-            b = random.choice(data)
-            name_b = b["name"]
-            description_b = b["description"]
-            country_b = b["country"]
-            followers_b = b["follower_count"]
-            print(f"You're right! Current score: {score}")
-        else:
-            #Clear screen & end loop
-            print(f"Sorry, that's wrong. Finale score: {score}")
-            endgame = True
-
-
-
-
-
-
-
+    if a == b:
+        b = random.choice(data)
+        name_b = b["name"]
+        description_b = b["description"]
+        country_b = b["country"]
+        followers_b = b["follower_count"]
+    else:
+        print(art.logo)
+        print(f"Compare A: {name_a}, {description_a}, from {country_a}, answer = {followers_a}")
+        print(art.vs)
+        print(f"Compare B: {name_b}, {description_b}, from {country_b}, answer = {followers_b}")
+        guess = input("Who has more followers? Type 'A' or 'B' ").lower()
+        if guess == "a":
+            if followers_a > followers_b:
+                score += 1
+                replace_a(a)
+                b = random.choice(data)
+                update_a(a,b)
+                cls()
+                print(f"You're right! Current score: {score}")
+            else:
+                cls()
+                print(f"Sorry, that's wrong. Finale score: {score}")
+                endgame = True
+        elif guess == "b":
+            if followers_b > followers_a:
+                score += 1
+                replace_a(a)
+                b = random.choice(data)
+                update_a(a,b)
+                cls()
+                print(f"You're right! Current score: {score}")
+            else:
+                cls()
+                print(f"Sorry, that's wrong. Finale score: {score}")
+                endgame = True
